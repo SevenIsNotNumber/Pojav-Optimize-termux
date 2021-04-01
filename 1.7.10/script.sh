a33d9f669cb3ef2c2fd50d6e55211d9d8aa23a54
SevenIsNotNumber <69080322+SevenIsNotNumber@users.noreply.github.com>
2021-04-01 17:47:00 +0800

    Create script.sh

---
+++ 1.7.10/script.sh
@@ -0,0 +1,17 @@
+cd /sdcard/games/PojavLauncher/.minecraft/mods
+echo "your currently optimizing 1.7.10"
+# optiFine
+wget https://optifine.net/downloadx?f=OptiFine_1.7.10_HD_U_E7.jar&x=3a04f26a49cb296b02f411bf8a5b9c57
+# BetterFps
+wget https://www.curseforge.com/minecraft/mc-mods/betterfps/files/2235429
+# FpsReducer
+wget https://www.curseforge.com/minecraft/mc-mods/fps-reducer/download/3103648/file
+# Done :)
+echo "Done:)"
+echo "Tips to optimize game:"
+echo "Lower Down the Render Distance"
+echo "Atleast 2gb to run 1.7.10 smoothly. Any below would be slow"
+echo "U can reduce resolution of game with Pojav Settings"
+echo "Use play store version"
+echo "Its important to give your OS plenty of RAM. Ram shouldn't be too low or too high"
+echo "Learn more on my channel: Py10 Playz."
